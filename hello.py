b713abf2a7b248e2cc002ee548b28ee6928ff6a9
ZihanYe <cloverlemo@hotmail.com>
2017-11-17 23:37:39 +0000

    hello.py update 11:37

--- hello.py
+++ hello.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask import Response
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/readdata/<country>/<age>/<year>/<index>",methods=['GET'])
+def readdata(country,age,year,index):
+    data = country
+    return data
+
+
+if __name__ == "__main__":
+	app.run() 
