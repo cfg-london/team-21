05708e21e7271d5b43ca51f9539515968e63f9d3
Sam McArdle <sammcardle30@gmail.com>
2017-11-17 22:48:04 +0000

    Updated json

--- json_io.py
+++ json_io.py
@@ -1,9 +1,24 @@
+from flask import Response
 from flask import Flask
+import json 
+
 app = Flask(__name__)
 
-@app.route("/")
-def output():
-	return "Hello Sam"
+
+@app.route('/readdata/<country>', methods = ['GET'])
+def api_hello(country):
+    data = {
+        'hello'  : 'world',
+        'number' : 3
+    }
+    js = json.dumps(data)
+
+    resp = Response(country + js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://luisrei.com'
+
+    return resp
 
 if __name__ == "__main__":
 	app.run()
+
+
