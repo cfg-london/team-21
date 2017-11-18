8a1137a1e2f0358ac2d2f6409d02f60e796df0fb
ZihanYe <cloverlemo@hotmail.com>
2017-11-18 02:06:04 +0000

    change the hello.py

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import Response
 import json
 import getSingleValue
+import indicatorMap
 
 app = Flask(__name__)
 
@@ -12,8 +13,8 @@ def hello():
 
 @app.route("/readdata/<country>/<index>/<age>/<year>",methods=['GET'])
 def readdata(country,age,year,index):
-    
-    data = getSingleValue.getvalue(index,courtry,year,age)
+    indicator = indicatorMap.dict_map[index]
+    data = json.dumps(getSingleValue.getvalue(indicator,courtry,year,age))
     return data
 
 
